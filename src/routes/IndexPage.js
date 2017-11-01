ee1eec2cb0538a8076996dab4dc3140593df322a
beautifulBoys <1298947916@qq.com>
2017-11-01 09:56:44 +0800

    12

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -11,7 +11,6 @@ function IndexPage({ location }) {
         <div className={styles.welcome} />
         <ul className={styles.list}>
           <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-          <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
         </ul>
       </div>
     </MainLayout>
